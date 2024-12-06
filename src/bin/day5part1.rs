12570ce5faedefc946b4989ea96ce594714a16cf
ben reed <ben.reed@sjsu.edu>
2024-12-05 20:56:42 -0800

    day 5

---
+++ src/bin/day5part1.rs
@@ -0,0 +1,53 @@
+use bcr33d_aoc_2024::MyIn;
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    println!("day5 part 1");
+    let mut myin = MyIn::new();
+    let mut total: i64 = 0;
+    let mut after: HashMap<i32, HashSet<i32>> = HashMap::new();
+    loop {
+        let line = myin.read_line();
+        if line.is_empty() {
+            break;
+        }
+        let parts: Vec<_> = line
+            .split("|")
+            .map(|s| s.parse::<i32>().unwrap_or(0))
+            .collect();
+        match after.get_mut(&parts[0]) {
+            Some(hs) => {
+                hs.insert(parts[1]);
+            }
+            None => {
+                let mut hs = HashSet::new();
+                hs.insert(parts[1]);
+                after.insert(parts[0], hs);
+            }
+        }
+    }
+    loop {
+        let line = myin.read_line();
+        if line.is_empty() {
+            break;
+        }
+        let parts: Vec<_> = line.split(",").map(|s| s.parse().unwrap_or(0)).collect();
+        let mut good = true;
+        'outer: for i in 0..parts.len() {
+            if let Some(hs) = after.get(&parts[i]) {
+                // println!("Checking {:#?} against {:?}", &parts[0..i], hs);
+                for j in 0..i {
+                    if hs.contains(&parts[j]) {
+                        good = false;
+                        break 'outer;
+                    }
+                }
+            }
+        }
+        if good {
+            let mid = parts[parts.len() / 2] as i64;
+            total += mid;
+        }
+    }
+    println!("{}", total);
+}
